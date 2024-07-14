102cbeead592768e7b1b9d8dce040351e6277df9
cikeuro <cikeuro@gmail.com>
2024-07-14 10:40:29 +0800

    update depan

--- src/Gomo.h
+++ src/Gomo.h
@@ -9,7 +9,7 @@ class Gomo
 		public :
 			Gomo(int INA1, int INA2,int PWMA, int INB1,int INB2, int PWMB, int STBY);
 			void begin();
-			void Depan();
+			void Depan3();
 			void Belakang();
 			void Kiri();
 			void Kanan();
